8d0aa141f93f324284eda9c47da47b28df1b6a50
Dea Bočina <140092973+deabocina@users.noreply.github.com>
2024-10-17 23:52:12 +0200

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import "./App.css";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { WeatherData } from "./types/WeatherData";
 import { getFormattedDateTime } from "./utils/dateUtils";
 import Search from "./components/Search";
@@ -10,6 +10,9 @@ import { fetchWeatherData } from "./services/weatherServices";
 function App() {
   const [location, setLocation] = useState<string>("");
   const [weatherData, setWeatherData] = useState<WeatherData | null>(null);
+  const [isDayTime, setIsDayTime] = useState<boolean>(true);
+  const [fade, setFade] = useState<boolean>(false);
+  const [isCelsius, setIsCelsius] = useState<boolean>(true);
 
   const handleOnSearchChange = async (searchData: string) => {
     setLocation(searchData);
@@ -19,24 +22,65 @@ function App() {
     }
   };
 
-  const videoOption =
-    weatherData?.current.is_day === 1
-      ? "/background-day.mp4"
-      : "/background-night.mp4";
+  const toggleUnits = () => {
+    setIsCelsius(!isCelsius);
+  };
+
+  useEffect(() => {
+    if (weatherData) {
+      const newIsDayTime = weatherData.current.is_day === 1;
+
+      if (newIsDayTime !== isDayTime) {
+        setFade(true);
+
+        setTimeout(() => {
+          setIsDayTime(newIsDayTime);
+          setFade(false);
+        }, 500);
+      }
+    }
+  }, [weatherData, isDayTime]);
+
+  useEffect(() => {
+    if (weatherData) {
+      setIsDayTime(weatherData.current.is_day === 1);
+    }
+  }, [weatherData]);
 
   return (
     <div>
-      <Search onSearchChange={handleOnSearchChange} />
       <div>
+        <Search
+          onSearchChange={handleOnSearchChange}
+          isCelsius={isCelsius}
+          toggleUnits={toggleUnits}
+        />
+      </div>
+
+      <div className="video-container">
         <video
-          key={videoOption}
           autoPlay
           loop
           muted
           playsInline
-          className="background-video"
+          className={`background-video day-video ${
+            !fade && isDayTime ? "fade-in" : "fade-out"
+          }`}
         >
-          <source src={videoOption} type="video/mp4" />
+          <source src="/background-day.mp4" type="video/mp4" />
+          Your browser does not support the video tag.
+        </video>
+
+        <video
+          autoPlay
+          loop
+          muted
+          playsInline
+          className={`background-video night-video ${
+            !fade && !isDayTime ? "fade-in" : "fade-out"
+          }`}
+        >
+          <source src="/background-night.mp4" type="video/mp4" />
           Your browser does not support the video tag.
         </video>
       </div>
@@ -45,11 +89,13 @@ function App() {
         <CurrentWeather
           weatherData={weatherData}
           getFormattedDateTime={getFormattedDateTime}
+          isCelsius={isCelsius}
         />
 
         <WeatherForecast
           weatherData={weatherData}
           getFormattedDateTime={getFormattedDateTime}
+          isCelsius={isCelsius}
         />
       </div>
 
